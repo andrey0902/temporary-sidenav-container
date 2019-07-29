8d1db96a3dcbb472e5a8af5c91ecd4a986ca0bb6
andrey0902 <andreypavlov1706@gmail.com>
2019-07-29 23:07:56 +0300

    fixed and remove more tests

--- src/app/engagement-type/ink-bar.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InkBarDirective } from './ink-bar.directive';
-
-describe('InkBarDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InkBarDirective();
-    expect(directive).toBeTruthy();
-  });
-});
